8e3ba73cde448ba6cadf2e8e6bd354e85b147e1a
sbrunkovskyi <sbrunkovskyi@ngoar.net>
2020-10-09 23:15:10 +0300

    commit30

--- wallbler-rest/src/main/java/com/nkoad/wallbler/rest/FeedWallblerRest.java
+++ wallbler-rest/src/main/java/com/nkoad/wallbler/rest/FeedWallblerRest.java
@@ -1,15 +1,19 @@
 package com.nkoad.wallbler.rest;
 
+import com.nkoad.wallbler.exception.AccountAlreadyExistsException;
 import com.nkoad.wallbler.service.OsgiConfigurationService;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
+import static javax.ws.rs.core.Response.status;
 
 @Path("/wallbler")
 @Component(name = "Feed Wallbler Rest Service", service = FeedWallblerRest.class, property = {"osgi.jaxrs.resource=true"})
@@ -31,6 +35,25 @@ public class FeedWallblerRest {
     @GET
     public List<Map<String, Object>> getFeeds() {
         return osgiService.readFeeds();
+    }
+
+    // Create a new feed
+    @Path("/feed")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    @POST
+    public Response createFeed(HashMap<String, Object> config) {
+        try {
+            return status(201).entity(osgiService.create(config)).build();
+        } catch (AccountAlreadyExistsException e) {
+            return status(409).entity(generateErrorMessage(e.getMessage())).build();
+        } catch (IOException e) {
+            return status(500).build();
+        }
+    }
+
+    private String generateErrorMessage(String message) {
+        return String.format("{\"message\":\"%s\"}", message);
     }
 
 }
