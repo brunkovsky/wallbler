21e1e2ae34489343118341199ac43a87cb8278b0
sbrunkovskyi <sbrunkovskyi@ngoar.net>
2020-10-22 22:44:55 +0300

    commit55

--- wallbler-core/src/main/java/com/nkoad/wallbler/core/implementation/facebook/FacebookValidator.java
+++ wallbler-core/src/main/java/com/nkoad/wallbler/core/implementation/facebook/FacebookValidator.java
@@ -3,6 +3,7 @@ package com.nkoad.wallbler.core.implementation.facebook;
 import com.nkoad.wallbler.httpConnector.GETConnector;
 import com.nkoad.wallbler.core.HTTPRequest;
 import com.nkoad.wallbler.core.Validator;
+import org.json.JSONObject;
 
 import java.net.URLEncoder;
 import java.util.Map;
@@ -21,12 +22,14 @@ public class FacebookValidator extends Validator {
             String url = USERS_API_ACCESS_URL + properties.get("config.groupId") + "?access_token=" + accessToken;
             HTTPRequest httpRequest = new GETConnector().httpRequest(url);
             if (httpRequest.getStatusCode() == 200) {
+                String name = new JSONObject(httpRequest.getBody()).getString("name");
+                LOGGER.info("facebook account is valid. account name: " + properties.get("config.name") + ". gotten account name: " + name);
                 return true;
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
-        LOGGER.info("facebook account is not valid");
+        LOGGER.warn("facebook account is not valid. account name: " + properties.get("config.name"));
         return false;
     }
 
